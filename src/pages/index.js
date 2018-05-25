45784788fe9ff201a536da67ace55234d2599642
Phong <phongvh50ca@gmail.com>
2018-05-25 15:15:09 +0700

    modify Homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
 import React from "react"
+import Link from 'gatsby-link'
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => (
+  <div>
+    <h1 style={{ color: "red" }}>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <img src="https://source.unsplash.com/random/600x400" alt="" />
+    <p>Now go build something great.</p>
+    <div>
+      <Link to="/blog/my-first-post">My first Post</Link>
+    </div>
+    <Link to="/second_post">Second Post</Link>
+  </div>
+)
+
+export default IndexPage
